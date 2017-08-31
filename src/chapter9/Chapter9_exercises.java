50788f083f0a8555a39f994b9e38f225d50ad604
devLearner01 <devLearner01@gmail.com>
2017-08-31 22:36:00 +0100

    added openChaining search + added some more logging.

--- src/chapter9/Chapter9_exercises.java
+++ src/chapter9/Chapter9_exercises.java
@@ -19,100 +19,148 @@ import devLearner01_simpleJava.GlobalConstants;
 public class Chapter9_exercises {
 
 	public Logger log;// = new Logger("Chapter9_exercises...");
-	
+
 	public void ex_for_section_9Point1_selfcheck_1() {
 
 		Set<String> s = new HashSet<String>();
-		s.add("hello");
-		s.add("bye");
-		s.addAll(s);
+		s.add( "hello" );
+		s.add( "bye" );
+		s.addAll( s );
 
 		Set<String> t = new TreeSet<String>();
-		t.add("123");
-		t.addAll(t);
+		t.add( "123" );
+		t.addAll( t );
 
-		System.out.println(s.containsAll(t));
-		System.out.println(t.containsAll(s));
-		System.out.println(s.contains("ace"));
-		System.out.println(s.contains("123"));
-		s.retainAll(t);
-		System.out.println(s.contains("123"));
-		s.retainAll(s);
-		System.out.println(s.contains("123"));
+		System.out.println( s.containsAll( t ) );
+		System.out.println( t.containsAll( s ) );
+		System.out.println( s.contains( "ace" ) );
+		System.out.println( s.contains( "123" ) );
+		s.retainAll( t );
+		System.out.println( s.contains( "123" ) );
+		s.retainAll( s );
+		System.out.println( s.contains( "123" ) );
 
 	}
 
 	/**
 	 * this method counts every word of the given text
-	 * @param filePath the file to be scanned
+	 * 
+	 * @param filePath
+	 *            the file to be scanned
 	 */
-	public void ex_for_section_9Point2_programming_1( String filePath) {
+	public void ex_for_section_9Point2_programming_1( String filePath ) {
 
 		Map<String, Integer> countWords = new HashMap<String, Integer>();
 
 		// read from file
-		try( BufferedReader input = new BufferedReader(new FileReader(new File(filePath )))){
+		try ( BufferedReader input = new BufferedReader( new FileReader( new File( filePath ) ) ) ) {
 
 			String line;
-			while( (line = input.readLine()) != null ) {
-				
-				String[] lineWords = line.split(" ");
-				
-				for(String word : lineWords) {
-					
-					if( countWords.containsKey(word.toLowerCase())) {
+			while ( ( line = input.readLine() ) != null ) {
+
+				String[] lineWords = line.split( " " );
+
+				for ( String word : lineWords ) {
+
+					if ( countWords.containsKey( word.toLowerCase() ) ) {
 						int value = countWords.get( word.toLowerCase() );
-						countWords.put(word.toLowerCase(), ++value);
-					}
-					else {
+						countWords.put( word.toLowerCase(), ++ value );
+					} else {
 						countWords.put( word.toLowerCase(), 1 );
 					}
 				}
 			}
-			
-			printHashMap( countWords );	
-			writeResToFile( countWords );	
-		}  
-		catch (IOException e) {
-			e.printStackTrace( );
-			log.info("couldn't read from input file or couldn't create result file.");
+
+			printHashMap( countWords );
+			writeResToFile( countWords );
+		} catch ( IOException e ) {
+			e.printStackTrace();
+			log.info( "couldn't read from input file or couldn't create result file." );
 		}
 	}
 
-	
 	private void printHashMap( Map<String, Integer> hm ) {
-		
-		for( Entry<String, Integer> e : hm.entrySet()) {
-			
-			if ( e.getKey() != null)
+
+		for ( Entry<String, Integer> e : hm.entrySet() ) {
+
+			if ( e.getKey() != null )
 				System.out.println( e.getKey() + " : " + e.getValue() + " time(s)." );
 		}
 	}
-	
-	private void writeResToFile( Map<String, Integer> hm  ) throws IOException{
 
-		
+	private void writeResToFile( Map<String, Integer> hm ) throws IOException {
+
 		File fResFile = new File( GlobalConstants.RESULT_FILE );
 		fResFile.createNewFile();
-		
-		
+
 		try {
-			BufferedWriter output = new BufferedWriter( new FileWriter( fResFile ));
-			
-			for( Entry<String, Integer> e : hm.entrySet()) {
-				
-				if ( e.getKey() != null)
+			BufferedWriter output = new BufferedWriter( new FileWriter( fResFile ) );
+
+			for ( Entry<String, Integer> e : hm.entrySet() ) {
+
+				if ( e.getKey() != null )
 					output.write( e.getKey().toLowerCase() + " : " + e.getValue() );
-					output.newLine();	
+				output.newLine();
 			}
 			output.close();
-	
-		} 
-		catch ( IOException e2 ) {
+
+		} catch ( IOException e2 ) {
 			e2.printStackTrace();
 			log.info( "error while writing result to file." );
 		}
 	}
 
-	
+	/**
+	 * @requires valueToLookFor != null
+	 * @param hashTable
+	 * @param key
+	 */
+	public int ex_for_section_9Point3_programming_1( String[] hashTable, String key ) {
+
+		// 1.
+		int index = key.hashCode() % hashTable.length;
+		//make the index positive
+		if ( index < 0 )
+			index += hashTable.length;
+
+		// 2. check immediate result
+		if ( hashTable[index] == null )
+			return -1;
+		else if ( hashTable[index].equals( key ) )
+			return index;
+
+		// 3. find with openChaining protocol
+		else
+			return this.openChainingFind( hashTable, key, index );
+	}
+
+	private int openChainingFind( String[] hashTable, String valueToLookFor, int index) {
+
+		int res = 999;
+		int firstCheckedIndex = valueToLookFor.hashCode() % hashTable.length;
+		
+		while ( res != 0 && res != - 1 ) {
+
+			index ++ ;
+			
+			//got to the end of the table - wrap around
+			if ( index >= hashTable.length )
+				index = 0;
+			
+			//scanned the whole table
+			if( index == firstCheckedIndex )
+				res = -1;
+			
+			if ( hashTable[index] == null )
+				res = - 1;
+			
+			else if ( hashTable[index].equals( valueToLookFor ) ) 
+				return index;
+				
+			else
+				res = 999;			
+		}
+		return res;
+	}
+
 }
--- src/controllers/Chapter9Controller.java
+++ src/controllers/Chapter9Controller.java
@@ -1,15 +1,52 @@
 package controllers;
 
+import java.util.logging.Logger;
+
 import chapter9.Chapter9_exercises;
 import devLearner01_simpleJava.GlobalConstants;
 
 public class Chapter9Controller {
+	
+	private final static Logger log = Logger.getLogger( Chapter9Controller.class.getName() );
+	private String[] hashTable;
 
+	
+	public Chapter9Controller() {
+		hashTable = new String[10];
+	}
+	
 	public void execute() {
 
 		Chapter9_exercises ch9ex = new Chapter9_exercises();
+		
 		ch9ex.ex_for_section_9Point1_selfcheck_1();
+		
 		ch9ex.ex_for_section_9Point2_programming_1( GlobalConstants.SAMPLE_TEXT_FILE );
+		
+		
+		log.info( "preparing for ch9ex.ex_for_section_9Point3_programming_1" );
+		
+		int index = "lookingForMe".hashCode() % hashTable.length;
+		if( index < 0 )
+			index += index + hashTable.length;
+		this.hashTable[ index ]  =  "lookingForMe" ;
+		
+		index = "anotherKey".hashCode() % hashTable.length;
+		if( index < 0 )
+			index += hashTable.length;
+		this.hashTable[ index ]  =  "anotherKey" ;
+		
+		int pos = ch9ex.ex_for_section_9Point3_programming_1( this.hashTable, "lookingForMe" );
+		if(pos == -1)
+			log.info( "key not found." );
+		else
+			log.info( "position where key is located: " + pos );
+		
+		pos = ch9ex.ex_for_section_9Point3_programming_1( this.hashTable, "anotherKey" );
+		if(pos == -1)
+			log.info( "key not found." );
+		else
+			log.info( "position where key is located: " + pos );
 	}
 
 }
